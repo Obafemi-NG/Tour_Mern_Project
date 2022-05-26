5ca9f6c944daac820afda4ad0a7f485bfdbfae09
obafemi <oludahunsiobafemi18@gmail.com>
2022-05-26 16:08:57 +0100

    made a significant correction to the form on the login page for better user experience

---
+++ client/src/components/CustomInput/CustomInput.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import styles from "./CustomInput.module.css";
+
+const CustomInput = ({ onChange, label, errorMsg, ...otherProps }) => {
+  const [focus, setFocus] = useState(false);
+  const onBlurHandler = () => {
+    setFocus(true);
+  };
+  return (
+    <div className={styles["input-section"]}>
+      <input
+        onBlur={onBlurHandler}
+        onChange={onChange}
+        focus={focus.toString()}
+        {...otherProps}
+      />
+      <label>{label}</label>
+      {focus && <p className={styles["error-msg"]}>{errorMsg}</p>}
+    </div>
+  );
+};
+
+export default CustomInput;
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,22 +2,48 @@ import React, { useState, useEffect } from "react";
 import styles from "./Login.module.css";
 import { ReactComponent as LoginIcon } from "../../assets/users-solid.svg";
 import { Link } from "react-router-dom";
+import CustomInput from "../../components/CustomInput/CustomInput";
 
 const initialState = {
   email: "",
   password: "",
 };
 const Login = () => {
+  const [focus, setFocus] = useState(false);
   const [formValue, setFormValue] = useState(initialState);
   const { email, password } = formValue;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setFormValue(initialState);
+    // setFormValue(initialState);
   };
   const handleChange = (e) => {
     setFormValue({ ...formValue, [e.target.name]: e.target.value });
   };
+  // const handleBlur = () => {
+  //   setFocus(true);
+  // };
+
+  const inputs = [
+    {
+      id: "email",
+      type: "email",
+      name: "email",
+      label: "Email",
+      errorMsg: "Invalid Email Address provided.",
+      required: true,
+      pattern: "[a-z0-9]+@[a-z]+.[a-z]{2,3}",
+    },
+    {
+      id: "password",
+      type: "password",
+      name: "password",
+      label: "Password",
+      errorMsg: "Invalid Password provided",
+      required: true,
+      pattern: "^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$",
+    },
+  ];
 
   return (
     <div className={styles["login-container"]}>
@@ -29,28 +55,48 @@ const Login = () => {
       </div>
 
       <div className={styles["input-container"]}>
-        <div className={styles["input-section"]}>
+        {inputs.map((input) => (
+          <CustomInput key={input.id} onChange={handleChange} {...input} />
+        ))}
+
+        {/* <div className={styles["input-section"]}>
           <input
             type="email"
             name="email"
             value={email}
+            onBlur={handleBlur}
             required
+            focus={focus.toString()}
             onChange={handleChange}
             pattern="[a-z0-9]+@[a-z]+.[a-z]{2,3}"
           />
           <label htmlFor="email"> Email </label>
+          {focus && (
+            <p className={styles["error-msg"]}>
+              {" "}
+              Invalid Email Address provided.{" "}
+            </p>
+          )}
         </div>
         <div className={styles["input-section"]}>
           <input
             type="password"
             name="password"
             value={password}
+            onBlur={handleBlur}
             onChange={handleChange}
+            focus={focus.toString()}
             required
             pattern="^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$"
           />
           <label htmlFor="email"> Password </label>
-        </div>
+          {focus && (
+            <p className={styles["error-msg"]}>
+              {" "}
+              Incorrect password provided.{" "}
+            </p>
+          )}
+        </div> */}
       </div>
       <div className={styles.footer}>
         <div className={styles.cta}>
